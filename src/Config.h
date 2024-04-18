1cdea1c162a90c8f0449c3bbd76addd9f42a3441
Pedro Elias <pedro.acse@gmail.com>
2024-04-18 15:49:01 +0100

    SF10

--- src/Config.h
+++ src/Config.h
@@ -10,8 +10,8 @@ const int loraIrqPin = 20;         // change for your board; must be a hardware 
 // LoRa configuration (Settings)
 const int loraTxPower = 20;          // LoRa Power setting (0-20)
 const long loraFrequency = 861E6;  // LoRa frequency in Hz
-const long loraSpreadingFactor = 7; // LoRa radio spreading factor
-const long loraBandwidth = 125E3; // LoRa radio bandwidth
+const long loraSpreadingFactor = 10; // LoRa radio spreading factor
+const long loraBandwidth = 500E3; // LoRa radio bandwidth
 const long loraSyncword = 0x2a; // ranges from 0-0x34, default 0x34
 const long loraGain = 0; // LoRa radio auto gain setting
 
